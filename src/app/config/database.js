8111270b2b27822243f1411b2de22b8c4421be46
Cold Dev <96265196+icemedia001@users.noreply.github.com>
2023-02-28 21:28:24 +0100

    Update database.js

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
-import { MONGO_URI } from process.env;
+const MONGO_URI = process.env.MONGO_URI;
+
 export const connect = () => {
 
     mongoose.set("strictQuery", false);
