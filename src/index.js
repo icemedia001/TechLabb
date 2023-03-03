24aece2f818bd470823306c14f4bc35085c689e0
Adeniyi Oluwateniayomi <you@example.com>
2023-03-03 18:47:23 +0100

    Updates forgot-password and reset-password.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import MongoDBStoreFactory from 'connect-mongodb-session';
-import Joi from "joi";
 const MongoDBStore = MongoDBStoreFactory(session);
 import helmet from "helmet";
 import multer from "multer";
