60c37c34ff57c5810fae7f58a46651ee2207bcd6
Adeniyi Oluwateniayomi <you@example.com>
2023-03-06 02:14:58 +0100

    Test API.

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router()
-import Book from "../models/book.js";
+import Book from "../models/Book.js";
 
 router.get('/', async (req, res) => {
   let books
